bed4e08655eda6de7291a3f4c723d575c5158794
Nathan Hinds <nathanphinds@gmail.com>
2025-08-31 23:28:32 -0500

    Fix asset paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      
+      base: '/unlock-thyself/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
